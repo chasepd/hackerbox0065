3015b73ce524df6a25b5d29fda7df38f721fb610
Chase Davies <chase.davies@sproutsocial.com>
2021-04-20 13:13:16 -0600

    Improved data server to make endpoint more generic, and to update coin prices on a timer instead of on every request

--- python/dataserver.py
+++ python/dataserver.py
@@ -1,13 +1,38 @@
 from flask import Flask
 from pycoingecko import CoinGeckoAPI
-import time
-import _thread
+from time import sleep
+from threading import Timer
 
 cg = CoinGeckoAPI()
 app = Flask(__name__)
 
 coin_data = {}
 
+#Credit for RepeatedTimer class goes to MestreLion from https://stackoverflow.com/questions/474528/what-is-the-best-way-to-repeatedly-execute-a-function-every-x-seconds
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
 
 def update_coin_data():
     prices = cg.get_price(ids=['bitcoin', 'ethereum', 'litecoin', 'monero'], vs_currencies='usd')
@@ -16,26 +41,15 @@ def update_coin_data():
     coin_data['litecoin'] = prices['litecoin']['usd']
     coin_data['monero'] = prices['monero']['usd']
 
+@app.route("/<coin>")
+def getPrice(coin):
+    try:
+        return str(coin_data[coin])
+    except KeyError:
+        return "NULL"
 
-@app.route("/bitcoin")
-def bitcoin():
-    update_coin_data()
-    return str(coin_data['bitcoin'])
-
-@app.route("/ethereum")
-def ethereum():
-    update_coin_data()
-    return str(coin_data['ethereum'])
-
-@app.route("/litecoin")
-def litecoin():
-    update_coin_data()
-    return str(coin_data['litecoin'])
-
-@app.route("/monero")
-def monero():
-    update_coin_data()
-    return str(coin_data['monero'])
 
 if __name__ == "__main__":
+    update_coin_data() #get initial values
+    rt = RepeatedTimer(10, update_coin_data) 
     app.run('0.0.0.0', 5000)
